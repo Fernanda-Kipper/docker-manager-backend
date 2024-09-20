22297d17a45bdcddab2ace56b9099c3f9deb9af9
Fernanda <nanda.kipper@gmail.com>
2024-09-20 12:59:59 -0300

    feat: init project

---
+++ src/main/java/com/fernandakipper/docker_manager/DockerManagerApplication.java
@@ -0,0 +1,13 @@
+package com.fernandakipper.docker_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fernandakipper/docker_manager/DockerManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernandakipper.docker_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
